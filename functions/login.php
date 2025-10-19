bd644cd05d4ddfac198f4fd1b93eab2b79e32813
Pajaarn <rizky.handani06@gmail.com>
2025-10-19 15:15:20 +0700

    Fix collision code, login & logout php

--- functions/login.php
+++ functions/login.php
@@ -13,7 +13,6 @@ $result = mysqli_query($conn, $query);
 if (mysqli_num_rows($result) > 0) {
     $data = mysqli_fetch_assoc($result);
 
-<<<<<<< HEAD
     // Simpan data user ke session
     $_SESSION['user_id'] = $data['user_id']; 
     $_SESSION['name'] = $data['name'];
@@ -30,27 +29,6 @@ if (mysqli_num_rows($result) > 0) {
         // header("Location: ../Hotel-Booking-System/index.php");
         header("Location: ../Hotel-Booking-System/index.php?login=success");
         exit();
-=======
-    // Verify password with password_verify
-    if (password_verify($password, $data['password_hash'])) {
-        // Simpan data user ke session
-        $_SESSION['name'] = $data['name'];
-        $_SESSION['role'] = $data['role'];
-        $_SESSION['status'] = 'login';
-
-        // Arahkan berdasarkan role
-        if ($data['role'] == 'admin') {
-            echo "<script>window.location.href = '../admin/index.php';</script>";
-            exit();
-        } elseif ($data['role'] == 'user') {
-            header("Location: ../Hotel-Booking-System/index.html");
-            exit();
-        } else {
-            $_SESSION['error'] = "Role tidak dikenal!";
-            header("Location: index.php");
-            exit();
-        }
->>>>>>> a1f8e3d30df41dec47899b0ffb65814281d0a79e
     } else {
         // Jika password salah
         $_SESSION['error'] = "Username atau Password salah!";
--- functions/logout.php
+++ functions/logout.php
@@ -1,23 +1,14 @@
 <?php
 session_start();
 
-<<<<<<< HEAD
-// Hapus semua session
-session_unset();
-session_destroy();
-
-header("Location: ../index.php");
-exit();
-?>
-=======
 // Unset all session variables
 $_SESSION = array();
 
 // Destroy the session
+session_unset();
 session_destroy();
 
 // Redirect to login or home page
-header("Location: login.php");
+header("Location: ../Hotel-Booking-System/index.php");
 exit();
 ?>
->>>>>>> a1f8e3d30df41dec47899b0ffb65814281d0a79e
